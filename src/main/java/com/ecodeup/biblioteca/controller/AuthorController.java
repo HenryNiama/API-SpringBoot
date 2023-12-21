7c232b01bf26ce020218f31b19a807a034f66dd7
Henry Niama <hsniama@utpl.edu.ec>
2023-12-20 22:59:09 -0500

    Creacion de metodos adicionales para el AuthorController.java

--- src/main/java/com/ecodeup/biblioteca/controller/AuthorController.java
+++ src/main/java/com/ecodeup/biblioteca/controller/AuthorController.java
@@ -5,10 +5,9 @@ import com.ecodeup.biblioteca.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v1/biblioteca")
@@ -21,7 +20,33 @@ public class AuthorController {
     //Metodo para guardar Author
     @PostMapping
     public ResponseEntity<AuthorDTO> save(@RequestBody AuthorDTO authorDTO){
-        return new ResponseEntity<>(authorService.saveAuthor(authorDTO), HttpStatus.OK);
+        return new ResponseEntity<>(authorService.saveAuthor(authorDTO), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List <AuthorDTO>> getAll(){
+        return new ResponseEntity<>(authorService.getAllAuthors(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getById(@PathVariable("id") Integer id){
+
+        try {
+            return new ResponseEntity<>(authorService.getAuthorById(id), HttpStatus.OK);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpStatus> deleteById(@PathVariable("id") Integer id){
+        try {
+            authorService.deleteAuthorById(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
 }
