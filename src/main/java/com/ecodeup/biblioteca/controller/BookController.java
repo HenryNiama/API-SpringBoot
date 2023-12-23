45e14999cb1d6e7e58494761494c9bf4c098fe3d
Henry Niama <hsniama@utpl.edu.ec>
2023-12-22 19:59:04 -0500

    Crear el metodo para buscar todos los libros.

--- src/main/java/com/ecodeup/biblioteca/controller/BookController.java
+++ src/main/java/com/ecodeup/biblioteca/controller/BookController.java
@@ -6,10 +6,9 @@ import com.ecodeup.biblioteca.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/biblioteca/books")
@@ -22,6 +21,17 @@ public class BookController {
     public ResponseEntity<BookDTO> save(@RequestBody BookDTO bookDTO){
         try {
             return new ResponseEntity<>(bookService.saveBook(bookDTO), HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAll(){
+        try {
+            return new ResponseEntity<>(bookService.getAllBooks(), HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
